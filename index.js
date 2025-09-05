ca1508a05e1a8e4775720d8c9e77807ee0a59d8a
Joao Pedro <joao.barbosa6@estudante.ifms.edu.br>
2025-09-04 21:30:00 -0400

    new commit, att

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("INFO 6A, A MELHOR, A MAIS MAIS")
+const express = require('express');
+const app = express()
+
+app.get('/', (req,res) =>{
+    res.render("Home") 
+})
+
+app.listen(3000, (err)=>{
+    console.log("A aplicação esta rodando na porta 3000")
+});
